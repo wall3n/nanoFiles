a6dc41a126cfad7787209f412ff313e3443d954e
Wall3n <franmoreno1506@gmail.com>
2025-04-29 13:28:26 +0200

    Feat: Inclusion de todos los valores del campo operation

--- nanoFilesP2Palumnos/src/es/um/redes/nanoFiles/udp/message/DirMessageOps.java
+++ nanoFilesP2Palumnos/src/es/um/redes/nanoFiles/udp/message/DirMessageOps.java
@@ -9,7 +9,16 @@ public class DirMessageOps {
 	 */
 	public static final String OPERATION_INVALID = "invalid_operation";
 	public static final String OPERATION_PING = "ping";
-	public static final String OPERATION_RESPONSE = "response";
+	public static final String OPERATION_PING_OK = "ping_ok";
+	public static final String OPERATION_BAD_PROTOCOL = "bad_protocol";
+	public static final String OPERATION_FILELIST_REQUEST = "filelist_request";
+	public static final String OPERATION_FILELIST_OK = "filelist_ok";
+	public static final String OPERATION_SERVE = "serve";
+	public static final String OPERATION_SERVE_OK = "serve_ok";
+	public static final String OPERATION_DOWNLOAD_REQUEST = "download_request";
+	public static final String OPERATION_DOWNLOAD_OK = "download_ok";
+	public static final String OPERATION_FILE_NOT_FOUND = "file_not_found";
+	public static final String OPERATION_FILE_AMBIGUOUS = "file_ambiguous";
 
 
 
