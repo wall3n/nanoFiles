37e3e6994266ee881c1764269209c40bfe132133
Wall3n <franmoreno1506@gmail.com>
2025-05-04 22:30:16 +0200

    Doc: Actualizacion de tareas

--- nanoFilesP2Palumnos/src/es/um/redes/nanoFiles/udp/message/DirMessageOps.java
+++ nanoFilesP2Palumnos/src/es/um/redes/nanoFiles/udp/message/DirMessageOps.java
@@ -3,7 +3,7 @@ package es.um.redes.nanoFiles.udp.message;
 public class DirMessageOps {
 
 	/*
-	 * TODO: (Boletín MensajesASCII) Añadir aquí todas las constantes que definen
+	 * DONE: (Boletín MensajesASCII) Añadir aquí todas las constantes que definen
 	 * los diferentes tipos de mensajes del protocolo de comunicación con el
 	 * directorio (valores posibles del campo "operation").
 	 */
