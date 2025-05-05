a7c933cfba369f28a5a02b32d781bd75c84c190a
Wall3n <franmoreno1506@gmail.com>
2025-05-05 06:10:36 +0200

    Feat: Cliente P2P implementado para la descarga de ficheros secuencial

--- nanoFilesP2Palumnos/src/es/um/redes/nanoFiles/tcp/client/NFConnector.java
+++ nanoFilesP2Palumnos/src/es/um/redes/nanoFiles/tcp/client/NFConnector.java
@@ -50,6 +50,7 @@ public class NFConnector {
 		 * DONE: (Boletín SocketsTCP) Enviar entero cualquiera a través del socket y
 		 * después recibir otro entero, comprobando que se trata del mismo valor.
 		 */
+		/*
 		try {
 			// Enviar
 			PeerMessage msgEnvia = new PeerMessage(PeerMessageOps.OPCODE_DOWNLOAD_FILE, "gsjsks".getBytes());
@@ -68,8 +69,62 @@ public class NFConnector {
 		} catch (IOException ex) {
 			System.out.println("Error en test(): " + ex.getMessage());
 		}
+		*/
 	}
 	
+	public String getFileHash(String fileName) throws IOException {
+		PeerMessage msgToPeer = new PeerMessage(PeerMessageOps.OPCODE_DOWNLOAD_FILE, (short) fileName.length(), fileName.getBytes());
+		msgToPeer.writeMessageToOutputStream(dos);
+		
+		PeerMessage msgFromPeer = PeerMessage.readMessageFromInputStream(dis);
+		if(msgFromPeer.getOpcode() == PeerMessageOps.OPCODE_FILE_NOT_FOUND){
+			System.err.println("File not found");
+			return null;
+		} else if(msgFromPeer.getOpcode() == PeerMessageOps.OPCODE_FILE_AMBIGUOUS){
+			System.err.println("File ambiguous");
+			return null;
+		} else if(msgFromPeer.getOpcode() == PeerMessageOps.OPCODE_FILE_FOUNDED) {
+			return new String(msgFromPeer.getFileName());
+		} else {
+			System.err.println("Unexpected error");
+			return null;
+		}
+		
+	}
+	
+	public long getFileSize() throws IOException {
+		PeerMessage msgToPeer = new PeerMessage(PeerMessageOps.OPCODE_GET_FILE_SIZE);
+		msgToPeer.writeMessageToOutputStream(dos);
+		
+		PeerMessage msgFromPeer = PeerMessage.readMessageFromInputStream(dis);
+		if(msgFromPeer.getOpcode() == PeerMessageOps.OPCODE_FILE_SIZE) {
+			return msgFromPeer.getFileSize();
+		} else {
+			System.err.println("getFileSize() dindt worked correctly");
+			return 0;
+		}
+	}
+	
+	public byte[] getChunk(long offset, int chunkSize) throws IOException {
+		PeerMessage msgToPeer = new PeerMessage(PeerMessageOps.OPCODE_GET_CHUNK, offset, chunkSize);
+		msgToPeer.writeMessageToOutputStream(dos);
+		
+		PeerMessage msgFromPeer = PeerMessage.readMessageFromInputStream(dis);
+		if(msgFromPeer.getOpcode() == PeerMessageOps.OPCODE_CHUNK_NOT_FOUND) {
+			System.err.println("Requested chunk: not found");
+			return null;
+		}
+		
+		return msgFromPeer.getData();
+	}
+	
+	public void transferEnd() throws IOException {
+		PeerMessage msgToPeer = new PeerMessage(PeerMessageOps.OPCODE_TRANSFER_END);
+		msgToPeer.writeMessageToOutputStream(dos);
+		dis.close();
+		dos.close();
+		socket.close();
+	}
 	
 
 
